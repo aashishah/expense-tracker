41a89a977b8f54d7d05b40f665ce3ed1b0e9ffcc
Aashi <aashishah0475@gmail.com>
2026-01-19 23:01:11 +0530

    'Edited'

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -16,8 +16,7 @@ export const AddTransaction= () => {
         <div className="form-control">
           <label for="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+            (negative - expense, positive - income)</label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}  placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
